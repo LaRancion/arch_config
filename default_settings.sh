a07125de7c6a7c798036750a521efeaba8353939
FL <me>
2024-01-27 11:36:25 +0100

    zsh modificata

--- default_settings.sh
+++ default_settings.sh
@@ -3,7 +3,6 @@
 #da aggiungere al file di configurazione dopo exec_always
 
 xrandr --output HDMI-2 --mode 1920x1080
-xrandr --output HDMI-2 --auto --above eDP-1
+xrandr --output HDMI-2 --auto --left-of eDP-1
 xrandr --output DP-1 --auto --left-of HDMI-2
 setxkbmap it
-
